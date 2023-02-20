53052216b13334db57bf98442898bf263aabecdc
Jennifer Blacutt <jennifer@blacutt.com>
2023-02-20 00:17:09 -0600

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <Container fluid className="bg-dark text-light text-center py-3">
+      <p>Jennifer Blacutt &copy; 2023</p>
+    </Container>
+  );
+}
+
+export default Footer;
