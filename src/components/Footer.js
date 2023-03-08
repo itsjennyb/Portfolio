1990e25ce03b773697a46e208455ac039aad550a
Jennifer Blacutt <jennifer@blacutt.com>
2023-03-08 15:54:02 -0600

    fix footer to stay on bottom of pg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 
 function Footer() {
   return (
-    <Container fluid className="bg-dark text-light text-center py-3">
+    <Container fluid className="bg-dark text-light text-center py-3" style={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
       <p>Jennifer Blacutt &copy; 2023</p>
     </Container>
   );
