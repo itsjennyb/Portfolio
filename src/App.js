0c57a3ded8c65f479251f318f4c40a06e010fa55
Jennifer Blacutt <jennifer@blacutt.com>
2025-01-10 13:24:26 -0600

    add asset files and create header component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header';
 
-import Home from './components/Home';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Contact from './pages/Contact';
 
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Home from './components/Home';
+// import About from './pages/About';
+// import Portfolio from './pages/Portfolio';
+// import Resume from './pages/Resume';
+// import Contact from './pages/Contact';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
 
 
 // function only for establishing routes/paths(urls)
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/resume' element={<Resume />} />
-      </Routes>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/about' element={<About />} />
+//         <Route path='/portfolio' element={<Portfolio />} />
+//         <Route path='/contact' element={<Contact />} />
+//         <Route path='/resume' element={<Resume />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
 // ===========================================================
 // * for later, personal pref *
