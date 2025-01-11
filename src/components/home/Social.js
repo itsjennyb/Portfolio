2f93b44ed68d750c2a6cf777e987cf3bedd50877
Jennifer Blacutt <jennifer@blacutt.com>
2025-01-10 18:28:18 -0600

    create homepage component

---
+++ src/components/home/Social.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className='home_social'>
+            <a href='https://github.com/itsjennyb' className='home_social-icon' target='_blank'>
+                <i class='uil uil-github-alt'></i>
+            </a>
+        
+            <a href='https://www.linkedin.com/in/jblac/' className='home_social-icon' target='_blank'>
+                <i class='uil uil-linkedin'></i>
+            </a>
+
+            {/* <a href='' className='home_social-icon' target='_blank'></a> */}
+
+        </div>
+    )
+}
+
+export default Social;
