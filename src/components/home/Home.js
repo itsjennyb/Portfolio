19b87c255be2b3dc0be7f4d06b9c91fe932bd4b6
Jennifer Blacutt <jennifer@blacutt.com>
2025-01-10 19:11:12 -0600

    add scroll icon and function

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './home.css';
 import Social from './Social';
 import Data from './Data';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
     return (
         <section className='home section' id='home'>
-            <div className='home_container container grid'></div>
+            <div className='home_container container grid'>
                 <div className='home_content grid'>
                     <Social />
 
@@ -14,6 +15,9 @@ const Home = () => {
 
                     <Data />
                 </div>
+
+                <ScrollDown />
+            </div>
         </section>
     )
 }
