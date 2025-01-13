0f312dd4172e351408149d12f53b5f9785684981
Jennifer Blacutt <jennifer@blacutt.com>
2025-01-13 12:31:33 -0600

    add skills component to page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-
-
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import Home from './components/Home';
-// import About from './pages/About';
-// import Portfolio from './pages/Portfolio';
-// import Resume from './pages/Resume';
-// import Contact from './pages/Contact';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 const App = () => {
   return (
@@ -20,59 +12,11 @@ const App = () => {
 
     <main className='main'>
       <Home/>
+      <About/>
+      <Skills/>
     </main>
     </>
   )
 }
-
-
-// function only for establishing routes/paths(urls)
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path='/' element={<Home />} />
-//         <Route path='/about' element={<About />} />
-//         <Route path='/portfolio' element={<Portfolio />} />
-//         <Route path='/contact' element={<Contact />} />
-//         <Route path='/resume' element={<Resume />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// ===========================================================
-// * for later, personal pref *
-// function LandingImage() {
-//   const [text, setText] = useState('');
-
-//   useEffect(() => {
-//     const textArray = ['is it me you\'re looking for?'];
-//     let currentIndex = 0;
-//     let currentText = '';
-
-//     const intervalId = setInterval(() => {
-//       if (currentIndex >= textArray.length) {
-//         clearInterval(intervalId);
-//       }
-
-//       currentText = textArray[currentIndex];
-//       setText(currentText.slice(0, text.length + 1));
-
-//       if (text.length === currentText.length) {
-//         clearInterval(intervalId);
-//       }
-//     }, 100);
-
-//     return () => clearInterval(intervalId);
-//   }, [text]);
-
-//   return (
-//     <div className="landing-image">
-//       <h1 className="landing-text">{text}</h1>
-//     </div>
-//   );
-// }
-
 
 export default App;
