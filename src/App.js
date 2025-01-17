0305dbf63864c10d5b0ed32ba18f8e48be9dcb29
Jennifer Blacutt <jennifer@blacutt.com>
2025-01-17 17:43:56 -0600

    add contact component, content and css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualifications from './components/qualifications/Qualifications';
-
+import Contact from './components/contact/Contact';
 
 const App = () => {
   return (
@@ -19,6 +19,7 @@ const App = () => {
       <Skills />
       <Services />
       <Qualifications />
+      <Contact />
     </main>
     </>
   ) 
