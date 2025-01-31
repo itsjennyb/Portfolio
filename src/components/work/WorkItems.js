76b2f532d89526821fffdd7847c65d623cabba12
Jennifer Blacutt <jennifer@blacutt.com>
2025-01-31 17:01:41 -0600

    add item cards under projects

--- src/components/work/WorkItems.js
+++ src/components/work/WorkItems.js
@@ -6,7 +6,7 @@ const WorkItems = ({ item }) => {
             <img src={item.image} alt='' className='work_img'></img>
             <h3 className='work_title'>{item.title}</h3>
             <a href='#' className='work_button'>
-
+                Demo <i className='bx bx-right-arrow-alt work_button-icon'></i>
             </a>
         </div>
     )
