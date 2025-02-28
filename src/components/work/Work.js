20c794509495e61bc8ca79067347de7255e368fa
Jennifer Blacutt <jennifer@blacutt.com>
2025-02-28 17:46:47 -0600

    add some text

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -6,7 +6,7 @@ const Work = () => {
     return (
         <section className='work section' id='portfolio'>
             <h2 className='section_title'>Portfolio</h2>
-            <span className='section_subtitle'>My projects</span>
+            <span className='section_subtitle'>Proof I can make things happen</span>
 
             <Projects />
         </section>
