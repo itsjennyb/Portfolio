cdb4f7215635dc09e76feb12e1193bad6a5c65b0
Jennifer Blacutt <jennifer@blacutt.com>
2025-02-28 17:42:09 -0600

    add and change some text

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,8 +4,8 @@ import './contact.css';
 const Contact = () => {
     return (
         <section className='contact section' id='contact'>
-            <h2 className='section_title'>Get in touch</h2>
-            <span className='section_subtitle'>Contact Me</span>
+            <h2 className='section_title'>Contact Me</h2>
+            <span className='section_subtitle'>Want to collaborate or offer a chance to work with your team?</span>
 
             <div className='contact_container container grid'>
                 <div className='contact_content'>
@@ -34,7 +34,7 @@ const Contact = () => {
                 </div>
 
                 <div className='contact_content'>
-                    <h3 className='contact_title'>Write me your project</h3>
+                    <h3 className='contact_title'>Write to me!</h3>
 
                     <form className='contact_form'>
                         <div className='contact_form-div'>
@@ -53,7 +53,7 @@ const Contact = () => {
                         </div>
 
                         <button className='button button--flex'>
-                        Send Message
+                        Send It
                             <svg
                             class="button__icon"
                             xmlns="http://www.w3.org/2000/svg"
