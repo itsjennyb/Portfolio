c6a1f335f8bbcd6949f0ab869d3170097a4f229a
Jennifer Blacutt <jennifer@blacutt.com>
2025-02-28 17:45:32 -0600

    change image file types

--- src/components/work/Data.js
+++ src/components/work/Data.js
@@ -1,40 +1,40 @@
-import Work1 from '../../assets/work1.jpg';
-import Work2 from '../../assets/work2.jpg';
-import Work3 from '../../assets/work3.jpg';
+import Work1 from '../../assets/proj1.jpg';
+import Work2 from '../../assets/proj2.jpg';
+import Work3 from '../../assets/proj3.jpg';
 
 
 export const projectsData =[
     {
         id: 1,
-        image: Work1,
-        title: 'Web design',
-        category: 'web',
+        image: Work3,
+        title: 'Date Review App',
+        category: 'apps',
     },
     {
         id: 2,
         image: Work2,
-        title: 'App',
-        category: 'app',
+        title: 'Busy Bee Task App',
+        category: 'apps',
     },
     {
         id: 3,
-        image: Work3,
-        title: 'Brand design',
-        category: 'design',
+        image: Work1,
+        title: 'Austin Foodies App',
+        category: 'apps',
     },
 ];
 
 export const projectsNav = [
+    // {
+    //     name: 'all',
+    // },
+    // {
+    //     name: 'web',
+    // },
     {
-        name: 'all',
+        name: 'apps',
     },
-    {
-        name: 'web',
-    },
-    {
-        name: 'app',
-    },
-    {
-        name: 'design',
-    },
+    // {
+    //     name: 'design',
+    // },
 ];
