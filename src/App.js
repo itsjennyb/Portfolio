6d15f59767f02fe869334362a31ff306822b081c
Jennifer Blacutt <jennifer@blacutt.com>
2025-02-28 17:50:03 -0600

    remove service component, save for later

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
-import Services from './components/services/Services';
+// import Services from './components/services/Services';
 import Qualifications from './components/qualifications/Qualifications';
 import Work from './components/work/Work';
 import Contact from './components/contact/Contact';
@@ -21,7 +21,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
-      <Services />
+      {/* <Services /> */}
       <Qualifications />
       <Work />
       <Contact />
